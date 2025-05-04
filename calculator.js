847d98d389f1df78aae16b8a4915e54b7059181e
Dan670-ai <danielyoyo2007@hotmail.com>
2025-05-04 14:20:44 +0800

    updated final ui design

--- calculator.js
+++ calculator.js
@@ -1,21 +1,34 @@
-let num1=0;
-let num2=0;
-let operator="";
 
+//num1,operator,num2,current_operation, result
+let current_numbers=["","","",0,0]
 //populate digit logic
 const digit_container=document.querySelector(".digits_grid")
-for (let i=0;i<9;i++){
+for (let i=1;i<10;i++){
     const button=document.createElement("button")
     button.classList.add("digits_button")
     button.textContent=i;
 
     button.addEventListener("click",()=>{
-        ButtonPressed(button);
+        DigitButtonPressed(button);
     })
     digit_container.appendChild(button)
 }
 
 //function logic
-function ButtonPressed(button){
+function DigitButtonPressed(button){
+    if (current_numbers[3]=0){
+        current_numbers[0]+=button.textContent;
+    }
+    if (current_numbers[3]=1){
+        current_numbers[1]+=button.textContent; 
+    }
+}
 
+function operate(){
+
+}
+
+
+const digital_display=document.querySelector(".display_result")
+function updateDisplay(){
 }
